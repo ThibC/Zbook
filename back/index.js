ef31a6a9d1bcaeb2829e339280304c5ae22aea31
Thibault Coudray <th.coudray@gmail.com>
2021-09-21 10:50:05 +0200

    Add POST /api/v1/meetings route

--- back/index.js
+++ back/index.js
@@ -6,10 +6,17 @@ const config = require('./config.json');
 const app = express();
 const BASE_URL = '/api/v1';
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.get(`${BASE_URL}/health-check`, ((req, res, next) =>
     res.status(200).send({ status: 'ok' })
 ));
+
+app.post(`${BASE_URL}/meetings`, ((req, res, next) => {
+    const data = req.body;
+    console.log(data);
+    res.status(200).send({ status: 'ok' });
+}));
 
 app.listen(config.express.PORT, () => console.log(`Server running on port ${config.express.PORT}`));
